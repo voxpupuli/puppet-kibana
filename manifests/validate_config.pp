ff724f21c5caa75f72e5bcf86e3845f717be6dd2
Funnelback hosting team <saas@funnelback.com>
2017-04-06 13:54:05 +1000

    Puppet linting

--- manifests/validate_config.pp
+++ manifests/validate_config.pp
@@ -1,13 +1,20 @@
+# Define: kibana::validate_config
+#
+# Validate that the config hash is a valid data type
+
 define kibana::validate_config(
   $config
 ) {
   $key = $title
-  
+
   if !(is_string($key) and length($key) > 0) {
     fail('config contains invalid keys')
   }
-  
-  if !is_integer($config[$key]) and !is_bool($config[$key]) and !is_array($config[$key]) and !(is_string($config[$key]) and  length($config[$key]) > 0) {
+
+  if !is_integer($config[$key]) and
+    !is_bool($config[$key]) and
+    !is_array($config[$key]) and
+    !(is_string($config[$key]) and length($config[$key]) > 0) {
     fail('config contains invalid values')
   }
 }
