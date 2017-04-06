dc62e0c137f7518ffa04262579fa8655de39d07a
Funnelback hosting team <saas@funnelback.com>
2017-04-06 14:14:08 +1000

    Should use size() instead of length() for Puppet 3.8

--- manifests/validate_config.pp
+++ manifests/validate_config.pp
@@ -7,14 +7,14 @@ define kibana::validate_config(
 ) {
   $key = $title
 
-  if !(is_string($key) and length($key) > 0) {
+  if !(is_string($key) and size($key) > 0) {
     fail('config contains invalid keys')
   }
 
   if !is_integer($config[$key]) and
     !is_bool($config[$key]) and
     !is_array($config[$key]) and
-    !(is_string($config[$key]) and length($config[$key]) > 0) {
+    !(is_string($config[$key]) and size($config[$key]) > 0) {
     fail('config contains invalid values')
   }
 }
